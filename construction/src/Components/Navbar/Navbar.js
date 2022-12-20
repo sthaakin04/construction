f02aef720f4c89a4266dabb426ffb15bab0b620d
sthaakin04 <akinstha97@gmail.com>
2022-12-20 16:25:32 +0545

    created project

---
+++ construction/src/Components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Navbar.css"
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+    const menu=(e) => {
+        let menu = document.querySelector('#menu-icon');
+        let navbar = document.querySelector('.navbar');
+        
+        menu.onclick = () => {
+          menu.classList.toggle('bx-x');
+          navbar.classList.toggle('open');
+        }
+      }
+  return (
+    <div>
+      <header>
+            {/* <Link to='#' className='logo'><img src="static/images/logo.png"/><span>Logo</span></Link> */}
+            {/* <Link to='/' className='logo'><img src="static/images/logotest.png"/></Link> */}
+            <ul className='navbar'>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+            <div className='main'>
+                <div className='bx bx-menu' id="menu-icon" onClick={menu}></div>
+            </div>
+        </header>
+    </div>
+  )
+}
