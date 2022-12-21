260b8df3dbbc66949674f31d2b8ca153f5e5e249
sthaakin04 <akinstha97@gmail.com>
2022-12-21 09:52:39 +0545

    home contents

--- construction/src/App.js
+++ construction/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/HomeContents/HomeContents'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ construction/src/Components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeContents from '../HomeContents/HomeContents'
+
+export default function Home() {
+  return (
+    <div>
+      <HomeContents/>
+    </div>
+  )
+}
